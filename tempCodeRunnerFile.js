ce6d608f61e58e50a9cc65f193a44b15f1f71c0f
Jhonatan <jhonatan.dev90@gmail.com>
2024-01-29 08:21:27 -0300

    Dados JS inseridos no arquivo JS, nao mais no HTML.

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const nota2 = Number(prompt("Digite a segunda nota: "));
-
-// const media = nota1 + nota2 / 2;
-
-// if (media >= 6) {
-//   console.log(`Média ${media}, o aluno foi APROVADO!`);
-// } else {
-//   console.log(`Média ${media}, o aluno foi REPROVADO`);
-// }
